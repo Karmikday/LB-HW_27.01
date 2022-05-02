77116f4a55fe48d28d3884c9a1c8d1398ae88c53
Tata_kn18 <karmikday@gmail.com>
2022-05-02 16:14:20 +0300

    done

--- main.cpp
+++ main.cpp
@@ -1,28 +1,29 @@
 #include <iostream>
 #include <time.h>
 using namespace std;
-int proverca(int* arr1,int* arr2,int size1,int size2)
+int* prov(int* A, int* B, int size1, int size2)
 {
-	int u = 0; 
+	int* u;
 	int i = 0;
 	for (i = 0; i < size1; i++)
 	{
 		int j = i, k = 0;
-		while (j < size1 && k < size2 && arr2[k] == arr1[j])
+		while (j < size1 && k < size2 && B[k] == A[j])
 		{
 			j++;
 			k++;
 		}
 		if (k == size2)
 		{
-			u = arr1 + i;
-			return u;
+			return u = A + i;
 		}
+	
 	}
-	return 0;
+	return 0; 
 }
 int main()
 {
+	srand(time(0));
 	int size1,size2;
 	cout << "Vvedite size 1 - >";
 	cin >> size1;
@@ -35,20 +36,18 @@ int main()
 	cout << "-----------------------------------" << endl;
 	for (size_t i = 0; i < size1; i++)
 	{
-		arr1[i] = rand() % 20;
-		cout << arr1[i] << endl;
+		cin >> arr1[i];
 	}
 	cout << "-----------------------------------" << endl;
 	cout << "ARRAY 2" << endl;
 	cout << "-----------------------------------" << endl;
 	for (size_t i = 0; i < size2; i++)
 	{
-		arr2[i] = rand() % 20;
-		cout << arr2[i] << endl;
+		cin >> arr2[i];
 	}
 	cout << "-----------------------------------" << endl;
 	cout << "RESULT" << endl;
 	cout << "-----------------------------------" << endl;
-	cout << "Resultat:" << proverca(arr1, arr2, size1, size2) << endl;
+	cout << "Resultat:" << *prov(arr1, arr2, size1, size2) << endl;
 
 }
